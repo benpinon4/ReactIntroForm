8457ae02565e61d5101146e8090943aedf736946
benpinon4 <benjamin.pinon@CodeImmersives.com>
2022-10-11 10:26:46 -0400

    Attempted stretch goals and was only able to get a dropdown working wit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App2 from "./AppPart2";
 
-test('renders learn react link', () => {
-  render(<App />);
+test("renders learn react link", () => {
+  render(<App2 />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
